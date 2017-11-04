f6263c8a10507fc41e92e71fb855487e4b7bac17
cristina1997 <cristinaflorinaeu@gmail.com>
2017-11-04 00:27:36 +0000

    Create 01-world-hello.go

---
+++ problem-sheet-1/01-world-hello.go
@@ -0,0 +1,17 @@
+//Q1. Kon’nichiwa, Sekai!
+	//Program that prints Hello, world! 
+	//in Japanese (using Japanese characters) 
+	//to the screen.
+
+	
+//Code addapted from
+	//https://golang.org/
+
+package main
+
+//imports paths from package main
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello, 世界") //outputs hello in japanese
+}//main
